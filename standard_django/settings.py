3493dba4426d0cc9aefdef25372a642b23f9b3f1
rahil <rahil@peerbits.com>
2016-12-07 12:16:44 +0530

    new commits

--- standard_django/settings.py
+++ standard_django/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'standard_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
